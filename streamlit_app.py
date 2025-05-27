b21d8b8a54bf607f5de162f6a54253e16688daaa
skay012 <pratamax0987@gmail.com>
2025-05-27 11:51:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 st.title("Selamat datang di server pawky world")
+st.write("server ini menceritakan tentang dua orang yang sedang membangun sebuah tempat yang dimana hanya ada mereka di dalam dunianya")
 st.write("bersama papaw :")
-st.image("IMG-20250527-WA0045.jpg") 
+st.image("IMG-20250527-WA0045.jpg")
 st.image("IMG-20250527-WA0048.jpg")
 st.write("dan kiky")
 st.image("IMG-20250527-WA0061.jpg")
 st.image("IMG-20250527-WA0060.jpg")
-st.title("# membangun cerita antara papaw dan kiky")
+st.title("# membangun kisah antara papaw dan kiky
