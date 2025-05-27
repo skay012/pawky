e0648ab4ef57b980c31c5bfa1e06ac9b40307e47
skay012 <pratamax0987@gmail.com>
2025-05-27 10:52:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Selamat datang di server pawky world")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-st. image("IMG_20250526_120735.jpg") 
+st.image("IMG_20250526_120735.jpg") 
