144bdccbe1de817eb80fa4d672e3a8bee1c74e58
skay012 <pratamax0987@gmail.com>
2025-05-27 11:19:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("Selamat datang di server pawky world")
 st.write("bersama papaw :")
 st.image("IMG-20250527-WA0045.jpg") 
-st.image("IMG-20250527-WA0048.jpg") 
+st.image("IMG-20250527-WA0048.jpg")
+
+st.write("bersama kiky :")
